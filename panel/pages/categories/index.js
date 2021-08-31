4b794a8422322ac2e97c130779b7bf8f2a95afb6
Alexandre Mrus <alexandre.azevedo.mrus@gmail.com>
2021-08-31 12:07:44 -0300

    Alert sem formatação

--- panel/pages/categories/index.js
+++ panel/pages/categories/index.js
@@ -40,64 +40,62 @@ const Index = () => {
         </Link>
       </div>
       <div className="flex flex-col mt-8">
-        {data && data.getAllCategories && data.getAllCategories.length === 0 && (
-          <div
-            className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
-            role="alert"
-          >
-            <p>Nenhuma categoria criada até o momento</p>
-          </div>
-        )}
-        {data && data.getAllCategories && data.getAllCategories.length > 0 && (
-          <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-            <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200"></div>
-            <Table>
-              <Table.Head>
-                <Table.Th>Categorias</Table.Th>
-                <Table.Th></Table.Th>
-              </Table.Head>
+        <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+          {data && data.getAllCategories && data.getAllCategories.length === 0 && (
+            <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4">
+              <p>Alerta sem formatação</p>
+            </div>
+          )}
+          {data && data.getAllCategories && data.getAllCategories.length > 0 && (
+            <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+              <Table>
+                <Table.Head>
+                  <Table.Th>Categorias</Table.Th>
+                  <Table.Th></Table.Th>
+                </Table.Head>
 
-              <Table.Body>
-                {data &&
-                  data.getAllCategories &&
-                  data.getAllCategories.map(item => {
-                    return (
-                      <Table.Tr key={item.id}>
-                        <Table.Td>
-                          <div className="flex items-center">
-                            <div>
-                              <div className="text-sm leading-5 font-medium text-gray-900">
-                                {item.name}
-                              </div>
-                              <div className="text-sm leading-5 text-gray-500">
-                                {item.slug}
+                <Table.Body>
+                  {data &&
+                    data.getAllCategories &&
+                    data.getAllCategories.map(item => {
+                      return (
+                        <Table.Tr key={item.id}>
+                          <Table.Td>
+                            <div className="flex items-center">
+                              <div>
+                                <div className="text-sm leading-5 font-medium text-gray-900">
+                                  {item.name}
+                                </div>
+                                <div className="text-sm leading-5 text-gray-500">
+                                  {item.slug}
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        </Table.Td>
-                        <Table.Td>
-                          <a
-                            href="#"
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            Edit
-                          </a>{' '}
-                          |{' '}
-                          <a
-                            href="#"
-                            className="text-red-600 hover:text-indigo-900"
-                            onClick={remove(item.id)}
-                          >
-                            Remove
-                          </a>
-                        </Table.Td>
-                      </Table.Tr>
-                    )
-                  })}
-              </Table.Body>
-            </Table>
-          </div>
-        )}
+                          </Table.Td>
+                          <Table.Td>
+                            <a
+                              href="#"
+                              className="text-indigo-600 hover:text-indigo-900"
+                            >
+                              Edit
+                            </a>{' '}
+                            |{' '}
+                            <a
+                              href="#"
+                              className="text-red-600 hover:text-indigo-900"
+                              onClick={remove(item.id)}
+                            >
+                              Remove
+                            </a>
+                          </Table.Td>
+                        </Table.Tr>
+                      )
+                    })}
+                </Table.Body>
+              </Table>
+            </div>
+          )}
+        </div>
       </div>
     </Layout>
   )
